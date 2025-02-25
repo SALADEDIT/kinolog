98c4333810b3709eb71118b957292f37a057c2a8
Julius Galler <himik711@mail.ru>
2025-02-25 18:34:30 +0300

    4.1 handleException Actor

--- src/main/java/ru/salad/ws/kinolog/controller/ActorController.java
+++ src/main/java/ru/salad/ws/kinolog/controller/ActorController.java
@@ -4,12 +4,15 @@ package ru.salad.ws.kinolog.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.salad.ws.kinolog.entity.Actor;
 import ru.salad.ws.kinolog.service.ActorService;
+import ru.salad.ws.kinolog.util.ErrorResponse;
+import ru.salad.ws.kinolog.util.NotFoundException;
 
 import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
 
@@ -22,6 +25,15 @@ public class ActorController {
     @GetMapping("/{id}")
     public ResponseEntity<Actor> findById(@PathVariable Long id) {
         return new ResponseEntity<>(service.findById(id), HttpStatus.OK);
+    }
+
+    @ExceptionHandler
+    private ResponseEntity<ErrorResponse> handleException(NotFoundException exception) {
+        ErrorResponse response = new ErrorResponse(
+            "Актёр с этим ID не найден",
+            System.currentTimeMillis()
+        );
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 
 }
