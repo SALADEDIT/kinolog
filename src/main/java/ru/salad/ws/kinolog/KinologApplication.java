799cbb3c30440191cc32b1817aed46e5d3fe6a86
Andrey Eliseev <andrey.s.eliseev@lenta.com>
2025-02-25 12:09:18 +0300

    Start project

---
+++ src/main/java/ru/salad/ws/kinolog/KinologApplication.java
@@ -0,0 +1,13 @@
+package ru.salad.ws.kinolog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinologApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinologApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/salad/ws/kinolog/KinologApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.salad.ws.kinolog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinologApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
